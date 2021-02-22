e664c088056608d6d99e6081343f0742300054ac
Alberto Zaranza <albertozaranza@outlook.com>
2021-02-22 11:05:00 -0300

    feat: start project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Move.it</Text>
+    </View>
+  );
+};
+
+export default App;
